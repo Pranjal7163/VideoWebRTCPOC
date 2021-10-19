35fa9bad9c0c2dba7c9011ba86cfe13f1729403a
Pranjal <pranjal.3vyas@gmail.com>
2021-10-19 12:38:38 +0530

    Full Screen Video

--- public/script.js
+++ public/script.js
@@ -25,13 +25,13 @@ var peer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
   port: "443",
-  config: {'iceServers': [
-    { url: 'stun:[stun.l.google.com:19302' },
-    { url: 'stun:[stun1.l.google.com:19302' },
-    { url: 'stun:[stun2.l.google.com:19302' },
-    { url: 'stun:[stun3.l.google.com:19302' },
-    { url: 'turn: 192.168.14.31:3478',username: 'amxadmin', credential: 'amx123' }
-    ]},
+  // config: {'iceServers': [
+  //   { url: 'stun:[stun.l.google.com:19302' },
+  //   { url: 'stun:[stun1.l.google.com:19302' },
+  //   { url: 'stun:[stun2.l.google.com:19302' },
+  //   { url: 'stun:[stun3.l.google.com:19302' },
+  //   { url: 'turn: 192.168.14.31:3478',username: 'amxadmin', credential: 'amx123' }
+  //   ]},
 });
 
 let myVideoStream;
