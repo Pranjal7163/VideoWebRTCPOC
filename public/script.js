46feb496fa4cf707d97c47761e5cc332519a84d1
Pranjal <pranjal.3vyas@gmail.com>
2021-10-25 09:26:56 +0530

    Error

--- public/script.js
+++ public/script.js
@@ -55,6 +55,7 @@ navigator.mediaDevices
     });
 
     socket.on('connection', function (socket) {
+      console.log('a user connected');
       socket.on('disconnect', function () {
           console.log(socket.id);
       });
