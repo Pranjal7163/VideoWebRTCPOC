e308194bbbecc627b5cf42ce396fd0bb6efa6c1d
Pranjal <pranjal7@msn.com>
2021-10-28 19:12:53 +0530

    Seperate Blobs

--- public/script.js
+++ public/script.js
@@ -126,8 +126,8 @@ function startRecording(){
     	// create local object URL from the recorded video blobs
     	// let video_local = URL.createObjectURL(new Blob(blobs_recorded, { type: 'video/webm' }));
     	// download_link.href = video_local;
-      blobs_recorded.forEach(blob => {
-        const url = URL.createObjectURL(new Blob(blob, { type: 'video/webm' }));
+      // blobs_recorded.forEach(blob => {
+        const url = URL.createObjectURL(new Blob(blobs_recorded, { type: 'video/webm' }));
         console.log(url);
         let a = document.createElement("a");
         a.style.display = "none";
@@ -136,7 +136,7 @@ function startRecording(){
         a.click();
         a.remove();
         URL.revokeObjectURL(url); 
-      })
+      // })
 
     });
 
